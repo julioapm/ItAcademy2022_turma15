e09d1207ade9dbb74278c241c4c003c45f121e1c
Júlio Pereira Machado <julioapm@gmail.com>
2022-05-10 17:25:10 -0300

    Demo de console

---
+++ csharp/DemoConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var diaHora = DateTime.Now;
+Console.WriteLine(diaHora);
+Console.WriteLine(diaHora.ToShortDateString());
+Console.WriteLine(diaHora.ToLongDateString());
+var amanha = diaHora.AddDays(1);
+Console.WriteLine(amanha);
