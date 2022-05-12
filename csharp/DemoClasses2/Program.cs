785e6f962a065cd80c06b345a82b4620bf53b5e3
Júlio Pereira Machado <julioapm@gmail.com>
2022-05-12 15:59:12 -0300

    DemoClasses2 passagem por referência versus valor

---
+++ csharp/DemoClasses2/Program.cs
@@ -0,0 +1,34 @@
+﻿int x = 10;
+int y = 3;
+Console.WriteLine(x/y);
+Console.WriteLine(x%y);
+var resultado = Math.DivRem(x,y);
+Console.WriteLine(resultado);
+(var q, var r) = Math.DivRem(x,y);
+Console.WriteLine(q);
+Console.WriteLine(r);
+int quociente;
+int resto = Math.DivRem(x,y,out quociente);
+Console.WriteLine(quociente);
+Console.WriteLine(resto);
+
+static void swap(ref int x, ref int y)
+{
+    var tmp = x;
+    x = y;
+    y = tmp;
+}
+
+static void swapvalor(int x, int y)
+{
+    var tmp = x;
+    x = y;
+    y = tmp;
+}
+
+int a = 1;
+int b = 2;
+//swap(ref a, ref b);
+swapvalor(a,b);
+Console.WriteLine(a);
+Console.WriteLine(b);
