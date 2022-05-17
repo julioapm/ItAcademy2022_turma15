53efd8899846cac37bffd24449a2cbbfc2335e95
Júlio Pereira Machado <julioapm@gmail.com>
2022-05-17 16:51:33 -0300

    DemoWSAloMundo logging

--- aspnet/DemoWSAloMundo/Controllers/WeatherForecastController.cs
+++ aspnet/DemoWSAloMundo/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.LogInformation("Foi realizada uma requisição GET");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
